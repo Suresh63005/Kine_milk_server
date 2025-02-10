6ec9a0198426c0eeb43fc460f859bcb08adbe3ef
Suresh63005 <193j5a0335@raghuinstech.com>
2025-02-10 13:56:04 +0530

    changes

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.use("/user",require('./UserRoutes/user_auth_route'))
 app.use("/customer",require('./UserRoutes/customer/customer_auth_routes'))
 
 
-app.use("/home_data", require("./UserRoutes/home_data_route"));
+app.use("/home_data", require("./UserControllers/Customer/home_date_controller"));
 app.use("/u_product", require("./UserRoutes/customer/product_route"));
 
 
