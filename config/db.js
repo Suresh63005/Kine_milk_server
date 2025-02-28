ccfa3b1500290a6ea9afa26b8e767782de057644
Suresh Pyla <122449185+Suresh63005@users.noreply.github.com>
2025-02-28 15:59:24 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT || 'mysql',
-    // dialectModule:require('mysql2'),
+    dialectModule:require('mysql2'),
     port: process.env.DB_PORT ,
     logging: console.log,
     pool: {
