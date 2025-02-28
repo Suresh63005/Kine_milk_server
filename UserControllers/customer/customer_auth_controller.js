ecb7cac9484ce5ca5b314862f29853f047d986f7
SureshPyla2000 <suresh.pyla@innoitlabs.com>
2025-02-28 19:03:41 +0530

    changesssssssssssssssssssss

--- UserControllers/customer/customer_auth_controller.js
+++ UserControllers/customer/customer_auth_controller.js
@@ -2,6 +2,12 @@ const User = require("../../Models/User");
 const asyncHandler = require("../../middlewares/errorHandler");
 const {customerFirebase} = require('../../config/firebase-config');
 const jwt = require('jsonwebtoken');
+const SubscribeOrder = require("../../Models/SubscribeOrder");
+const NormalOrder = require("../../Models/NormalOrder");
+const Address = require("../../Models/Address");
+const Favorite = require("../../Models/Favorite");
+const Cart = require("../../Models/Cart");
+
 
 const VerifyCustomerMobile = asyncHandler(async (req, res) => {
     let { mobile } = req.body;
@@ -152,4 +158,53 @@ const UpdateCustomerDetails = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { VerifyCustomerMobile,FetchCustomerDetails,UpdateCustomerDetails };
+const deleteCustomer = async (req, res) => {
+    
+    const  uid  = req.user.userId;
+
+    try {
+
+        const suborder = await SubscribeOrder.destroy({where:{uid}});
+        const normalOrder = await NormalOrder.destroy({where:{uid}});
+        const address = await Address.destroy({where:{uid}});
+        const fav = await Favorite.destroy({where:{uid}});
+        const cart = await Cart.destroy({where:{uid}});
+        
+   const customer = await User.destroy({ where: { id: uid } });
+
+   if(!customer){
+    return res.status(404).json({
+        ResponseCode: "404",
+        Result: "false",
+        ResponseMsg: "Customer not found",  
+
+        
+    })
+   }
+
+
+
+   
+
+   
+
+
+
+   return res.status(200).json({
+    ResponseCode: "200",
+    Result: "true",
+    ResponseMsg: "Customer deleted successfully",
+    });
+
+    } catch (error) {
+        console.error("Error deleting customer:", error.message);
+        return res.status(500).json({
+            ResponseCode: "500",
+            Result: "false",
+            ResponseMsg: "Internal server error: " + error.message,
+            });
+        
+    }
+}
+
+module.exports = { VerifyCustomerMobile,FetchCustomerDetails,UpdateCustomerDetails,deleteCustomer };
--- UserRoutes/customer_auth_routes.js
+++ UserRoutes/customer_auth_routes.js
@@ -5,6 +5,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post("/verify-customer",customerAuthController.VerifyCustomerMobile)
 router.get("/customer-details",authMiddleware.isAuthenticated,customerAuthController.FetchCustomerDetails)
-router.patch("/edit-customer",authMiddleware.isAuthenticated,customerAuthController.UpdateCustomerDetails)
+router.patch("/edit-customer",authMiddleware.isAuthenticated,customerAuthController.UpdateCustomerDetails);
+router.delete("/",authMiddleware.isAuthenticated,customerAuthController.deleteCustomer);
 
 module.exports = router
