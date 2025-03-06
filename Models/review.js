43295cdfcb44c371667fff46d8ef71506d262c9b
NARENDARKOREM <narendar.korem@innoitlabs.com>
2025-03-06 12:21:38 +0530

    changes

--- Models/review.js
+++ Models/review.js
@@ -15,6 +15,10 @@ const Review = sequelize.define(
         type: DataTypes.UUID,
         allowNull: false,
     },
+    store_id:{
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
     rider_id:{
       type: DataTypes.UUID,
       allowNull: false,
