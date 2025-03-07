2317ed48143d78a2ea5cd3e73c1f6bd7e3c3c9b8
SureshPyla2000 <suresh.pyla@innoitlabs.com>
2025-03-07 09:40:05 +0530

    changssssssssssssssss

--- UserRoutes/customer/fav_route.js
+++ UserRoutes/customer/fav_route.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/',isAuthenticated,addFavorite);
 router.get('/:store_id',isAuthenticated,getFavorites);
+
 router.delete('/',isAuthenticated,removeFavorite);
 
 
