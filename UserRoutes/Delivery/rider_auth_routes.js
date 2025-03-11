db44b786e0480d4407b8086c8d45fb60bc274ef2
NARENDARKOREM <narendar.korem@innoitlabs.com>
2025-03-11 10:04:09 +0530

    changessssssssss

--- UserRoutes/Delivery/rider_auth_routes.js
+++ UserRoutes/Delivery/rider_auth_routes.js
@@ -7,5 +7,7 @@ const upload = require('../../utils/multerConfig');
 router.post("/verify-rider",riderAuthController.VerifyRiderMobile)
 router.put("/edit-profile/:rider_id",authMiddleware.isAuthenticated,upload.single('img'),riderAuthController.EditRiderProfile)
 router.delete("/delete-rider-profile", authMiddleware.isAuthenticated,riderAuthController.DeleteRiderProfile);
+router.post("/add-onesignal",authMiddleware.isAuthenticated,riderAuthController.UpdateOneSignalSubscription);
+router.post("/remove-onesingal",authMiddleware.isAuthenticated,riderAuthController.RemoveOneSignalId);
 
 module.exports = router;
