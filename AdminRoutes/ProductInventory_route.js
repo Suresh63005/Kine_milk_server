2ee4530397ad2d3245cb3019303feaa5c440567d
diddiakhil <akhil.diddi@innoitlabs.com>
2025-04-08 18:25:50 +0530

    Changes

--- AdminRoutes/ProductInventory_route.js
+++ AdminRoutes/ProductInventory_route.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const ProductInventory = require('../AdminControllers/ProductInventory_controller');
 const upload = require('../utils/multerConfig');
-
+const adminMiddleware = require("../middlewares/adminMiddleware")
 const router = express.Router();
 
 
 
-router.post("/upsert",ProductInventory.upsertInventory);
+router.post("/upsert",adminMiddleware.isAdmin,ProductInventory.upsertInventory);
 
-router.post("/upsert-productinv",upload.none(),ProductInventory.upsertInventory);
-router.get("/getproductinv/:id",ProductInventory.getProductInventoryById);
-router.get("/getallproductinv",ProductInventory.ProductInventoryList)
-router.patch("/toggle-status/",ProductInventory.toggleProductInventoryStatus)
-router.delete("/delete/:id",ProductInventory.deleteProductInventory)
-router.get("/getproductbystore/:store_id",ProductInventory.getProductsbyStore)
-router.delete("/delete-weight-options",ProductInventory.deleteInventoryStoreWeightOptions)
+router.post("/upsert-productinv",adminMiddleware.isAdmin,upload.none(),ProductInventory.upsertInventory);
+router.get("/getproductinv/:id",adminMiddleware.isAdmin,ProductInventory.getProductInventoryById);
+router.get("/getallproductinv",adminMiddleware.isAdmin,ProductInventory.ProductInventoryList)
+router.patch("/toggle-status/",adminMiddleware.isAdmin,ProductInventory.toggleProductInventoryStatus)
+router.delete("/delete/:id",adminMiddleware.isAdmin,ProductInventory.deleteProductInventory)
+router.get("/getproductbystore/:store_id",adminMiddleware.isAdmin,ProductInventory.getProductsbyStore)
+router.delete("/delete-weight-options",adminMiddleware.isAdmin,ProductInventory.deleteInventoryStoreWeightOptions)
 
 module.exports = router;
--- AdminRoutes/stockRoutes.js
+++ AdminRoutes/stockRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const stockController = require('../AdminControllers/stockController');
+const adminMiddleware = require("../middlewares/adminMiddleware")
 
-router.get('/stock-reports', stockController.getStockReports);
-router.get('/stock-reports/download', stockController.downloadAllStockReports);
-router.get('/stock-reports/download/:storeId', stockController.downloadSingleStoreStockReport);
+router.get('/stock-reports',adminMiddleware.isAdmin, stockController.getStockReports);
+router.get('/stock-reports/download',adminMiddleware.isAdmin, stockController.downloadAllStockReports);
+router.get('/stock-reports/download/:storeId',adminMiddleware.isAdmin, stockController.downloadSingleStoreStockReport);
 
 module.exports = router;
